18145db64d10b10708b3ee1e7845fc1c7645df50
Ademar Junior <ademar.junior@mv.com.br>
2023-09-26 17:54:34 -0300

    iniciado projeto

---
+++ controller/src/main/java/br/com/junior/software/architect/integrationxls/IntegrationXlsApplication.java
@@ -0,0 +1,13 @@
+package br.com.junior.software.architect.integrationxls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationXlsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrationXlsApplication.class, args);
+	}
+
+}
